09ce46ed390eb4e43fdb8afaaf036e773c8c3e16
Akshata1712 <akmiramir17@gmail.com>
2025-10-18 21:23:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/Akshata1712.github.io/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
